dbdde746fad823ba517c8381fe4b317b681e788b
mparaiso <aikah@free.fr>
2015-04-05 21:14:37 +0200

    chat addr as cli argument

--- chat/main.go
+++ chat/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"path"
 	"sync"
+	"flag"
 )
 
 type templateHandler struct {
@@ -21,18 +22,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.tpl = template.Must(template.ParseFiles(path.Join("templates", t.filename)))
 	})
-	t.tpl.Execute(w, nil)
+	t.tpl.Execute(w, r)
 }
+
 func main() {
 	const (
 		port string = ":8080"
 	)
+	// deal with cli arguments
+	var addr = flag.String("addr",":8080","the addr of the application")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	//http.Handle("/public", http.FileServer(http.Dir("./public")))
 	go r.run()
-	if err := http.ListenAndServe(port, nil); err != nil {
+	log.Println("starting server on ",*addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
